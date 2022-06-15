d52c1b6f84209d3269bd863b3269db73a2ad0998
Filip <filip.ignjatovic12@gmail.com>
2022-06-15 18:03:22 +0200

    Osnovni koncept baze, migracije i baza

---
+++ Back/Web2Backend/DataAccess/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Admin> Admini { get; set; }
+        public DbSet<Dostavljac> Dostavljaci { get; set; }
+        public DbSet<Potrosac> Potrosaci { get; set; }
+        public DbSet<Porudzbina> Porudzbine { get; set; }
+        public DbSet<Proizvod> Proizvodi { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ServiceData.db");
+        }
+    }
+}
